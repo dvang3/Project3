cfe98eea42d2a62657818c30475e0eb50d4cc308
Anna Marie Estores <annamarieestores@Annas-MacBook-Pro.local>
2023-10-07 04:18:51 -0700

    Anna update

--- flask_blog/app.py
+++ flask_blog/app.py
@@ -2,24 +2,23 @@ import numpy as np
 import pandas as pd
 import sqlite3
 
-from flask import Flask, jsonify
-from flask import Flask, render_template
-
+from flask import Flask, render_template 
 #################################################
 # Flask Setup
 #################################################
 app = Flask(__name__)
 
-conn = sqlite3.connect('Resources/proj3.db')
+conn = sqlite3.connect('/Users/annamarieestores/Documents/Project3/flask_blog/Resources/proj3.sqlite')
 c = conn.cursor()
 data = c.execute('''SELECT * FROM crash_by_city''').fetchall() 
-
+data2 = c.execute('''SELECT * FROM fatal_crashes''').fetchall() 
+data3 = c.execute('''SELECT * FROM fatal_crashes_by_month''').fetchall() 
 #################################################
 # Flask Routes
 #################################################
 @app.route("/")
 def welcome():
-    return render_template('index.html',data=data)
+    return render_template("index.html", data=data,data2=data2,data3=data3)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- flask_blog/static/js/logic.js
+++ flask_blog/static/js/logic.js
@@ -1,37 +1,55 @@
-// const chart_data= {data,tojson}
-  
-// //    console.log (chart_data)
+//d3.json(chart_data).then(function(data) {
+//   console.log(data);
+// });
+// function init() {
+//   d3.json(chart_data).then((data) => {
+//     let area = data.county;
+//     let selector = d3.select("#selDataset");
 
-function init() {
-    d3.json(chart_data).then((data) => {
-      let counties = data.counties;
-      let selector = d3.select("#selDataset");
-  
-      for (let i = 0; i < counties.length; i++){
-        selector
-          .append("option")
-          .text(counties[i])
-          .property("value", counties[i]);
-      };
-        counties.forEach((id) => {
-        console.log(id);
-  
-        selector.append("option")
-        .text(id)
-        .property("value",id);
-      });
-  
-    //   let sample_name = samplenames[0];
-    //   buildCharts(sample_name);
-    //   buildMetadata(sample_name);
-    //   buildBubbleChart(sample_name);
-    });
-  };
-  function optionChanged (datavalue) {
-    // console.log(value); 
-  
-    buildCharts(datavalue);
-    buildMetadata(datavalue);
-    buildBubbleChart(datavalue);
-  };
-//   init();
+//     for (let i = 0; i < area.length; i++){
+//       selector
+//         .append("option")
+//         .text(area[i])
+//         .property("value", county[i]);
+//     };
+//       county.forEach((id) => {
+//       console.log(id);
+
+//       selector.append("option")
+//       .text(id)
+//       .property("value",id);
+//     });
+
+//     let area_name = area[0];
+//     buildCharts(area_name);
+//     buildMetadata(area_name);
+//     buildBubbleChart(area_name);
+//   });
+// };
+
+// init();
+
+// function buildMetadata(sample) {
+//   d3.json(bellybutton).then((data) => {
+
+//       let metadata = data.metadata;
+//       let value = metadata.filter(result => result.id == sample);
+//       console.log(value)
+
+//       let valueData = value[0];
+//       d3.select("#sample-metadata").html("");
+//       Object.entries(valueData).forEach(([key,value]) => {
+//       console.log(key,value);
+
+//       d3.select("#sample-metadata").append("h5").text(`${key}: ${value}`);
+//       });
+//   });
+// };
+
+// function optionChanged(value) {
+//   // console.log(value); 
+
+//   buildCharts(value);
+//   // buildMetadata(datavalue);
+//   // buildBubbleChart(datavalue);
+// };
