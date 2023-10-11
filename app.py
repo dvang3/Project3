ade33d2d2813c1d4c6e223ae447242ad2176a662
Anna Marie Estores <annamarieestores@Annas-MacBook-Pro.local>
2023-10-11 03:13:22 -0700

    nomore error update

--- app.py
+++ app.py
@@ -10,15 +10,14 @@ app = Flask(__name__)
 
 conn = sqlite3.connect('/Users/annamarieestores/Documents/Project3/flask_blog/Resources/proj3.sqlite')
 c = conn.cursor()
-data = c.execute('''SELECT * FROM crash_by_city''').fetchall() 
-data2 = c.execute('''SELECT * FROM fatal_crashes''').fetchall() 
-data3 = c.execute('''SELECT * FROM fatal_crashes_by_month''').fetchall() 
+data = c.execute('''SELECT * FROM crash_data''').fetchall() 
 #################################################
 # Flask Routes
 #################################################
 @app.route("/")
 def welcome():
-    return render_template("index.html", data=data,data2=data2,data3=data3)
 
+    return render_template ('index.html', data=data)
+    
 if __name__ == '__main__':
     app.run(debug=True)
--- flask_blog/static/js/logic.js
+++ flask_blog/static/js/logic.js
@@ -1,55 +1,96 @@
-//d3.json(chart_data).then(function(data) {
-//   console.log(data);
-// });
-// function init() {
-//   d3.json(chart_data).then((data) => {
-//     let area = data.county;
-//     let selector = d3.select("#selDataset");
+d3.csv("flask_blog/Resources/crash_data.csv", function(chart_data) {
+    
+    d3.select("body")
+        .selectAll("p")
+        .data(chart_data)
+        .enter()
+        .append("p")
+        .text(function(d) {
+            return d.county + ", " + d.value;
+        });
 
-//     for (let i = 0; i < area.length; i++){
-//       selector
-//         .append("option")
-//         .text(area[i])
-//         .property("value", county[i]);
-//     };
-//       county.forEach((id) => {
-//       console.log(id);
+});
+// Initialized arrays
+let county = []
+let total_fatal_crashes = []
+let total_injury_crashes = []
+let alcohol_involved_fatal = []
+let alcohol_involved_injury = []
 
-//       selector.append("option")
-//       .text(id)
-//       .property("value",id);
-//     });
+// For loop to populate arrays
 
-//     let area_name = area[0];
-//     buildCharts(area_name);
-//     buildMetadata(area_name);
-//     buildBubbleChart(area_name);
-//   });
-// };
+for (let i = 0; i < chart_data.length; i++) {
+  row = chart_data[i];
+  county.push(row.county);
+  total_fatal_crashes.push(row.alcohol_involved_injury);
+  total_injury_crashes.push(row.total_injury_crashes);
+  alcohol_involved_fatal.push(row.alcohol_involved_fatal);
+  alcohol_involved_injury.push(row.alcohol_involved_injury);
+};
 
-// init();
+// Trace1 for the Greek Data
+let trace1 = {
+  x: county,
+  y: total_fatal_crashes,
+  text: total_fatal_crashes,
+  name: "Total Fatal",
+  type: "bar"
+};
 
-// function buildMetadata(sample) {
-//   d3.json(bellybutton).then((data) => {
+// Trace 2 for the Roman Data
+let trace2 = {
+  x: county,
+  y: alcohol_involved_fatal,
+  text: alcohol_involved_fatal,
+  name: "Alcohol Fatal",
+  type: "bar"
+};
 
-//       let metadata = data.metadata;
-//       let value = metadata.filter(result => result.id == sample);
-//       console.log(value)
+let trace3 = {
+  x: county,
+  y: total_injury_crashes,
+  text: total_injury_crashes,
+  name: "Total Injury",
+  type: "bar"
+};
 
-//       let valueData = value[0];
-//       d3.select("#sample-metadata").html("");
-//       Object.entries(valueData).forEach(([key,value]) => {
-//       console.log(key,value);
+let trace4 = {
+  x: county,
+  y: alcohol_involved_injury,
+  text: alcohol_involved_injury,
+  name: "Alcohol Injury",
+  type: "bar"
+};
+// Create data array
+let resultdata = [trace1, trace2, trace3, trace4];
 
-//       d3.select("#sample-metadata").append("h5").text(`${key}: ${value}`);
-//       });
-//   });
-// };
+// Apply a title to the layout
+let layout = {
+  title: "California Crash Data Reports",
+  barmode: "group",
+  // Include margins in the layout so the x-tick labels display correctly
+  margin: {
+    l: 50,
+    r: 50,
+    b: 200,
+    t: 50,
+    pad: 4
+  }
+};
+function handleSubmit(event) {
+  event.preventDefault();
 
-// function optionChanged(value) {
-//   // console.log(value); 
+  const data = new FormData(event.target);
+  // const value = Object.fromEntries(data.entries());
 
-//   buildCharts(value);
-//   // buildMetadata(datavalue);
-//   // buildBubbleChart(datavalue);
-// };
+  const value = data.get('county');
+
+  console.log({ value });
+}
+
+const form = document.querySelector('form');
+form.addEventListener('submit', handleSubmit);
+// Render the plot to the div tag with id "plot"
+Plotly.newPlot("plot", chart_data, layout);
+
+
